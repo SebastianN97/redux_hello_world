f7661d3ce60a88a798da70230fa39c5d4fdec86f
SebastianN97 <sebbe132132@gmail.com>
2020-10-13 18:59:54 +0200

    Creating React app and cleaning up uneccessary files. Modifying App.js to jsx file. Setting up function component in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+        <Header as='h1'>Hello World</Header>
+      </Container>   
+    </div>
+  );
+}
+
+export default App;
