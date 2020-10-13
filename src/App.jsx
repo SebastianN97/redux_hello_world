5dd1c062f691806e3079158eb47b48e1b3d386dc
SebastianN97 <sebbe132132@gmail.com>
2020-10-13 19:08:17 +0200

    Adding cypress and cleaning up comments.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <Container>
-        <Header as='h1'>Hello World</Header>
-      </Container>   
+      <h1>Hello World</h1>
     </div>
   );
 }
